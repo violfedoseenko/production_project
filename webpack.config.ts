b851d53625c38c65e50faa50c6789d19bf863007
Violetta <viol.fedoseenko@yandex.ru>
2023-07-01 22:59:18 +0300

    dev server configured

--- webpack.config.ts
+++ webpack.config.ts
@@ -9,23 +9,28 @@ import { buildPlugins } from './config/build/buildPlugins'
 import { buildLoaders } from './config/build/buildLoaders'
 import { buildResolvers } from './config/build/buildResolvers'
 import { buildWebpackConfig } from './config/build/buildWebpackConfig'
-import { BuildPath } from './config/build/types/config'
+import { BuildEnv, BuildPath } from './config/build/types/config'
 
-export const paths: BuildPath = {
-	entry: path.resolve(__dirname, 'src', 'index.ts'), // склеиваем участки пути (__dirname - текущая папка)
-	output: path.resolve(__dirname, 'build'), //куда сохраняется
-	html: path.resolve(__dirname, 'public', 'index.html'), // при создании index.html в build используем шаблон из папки public файл index.html
+
+
+
+export default (env: BuildEnv)=> {
+	const paths: BuildPath = {
+		entry: path.resolve(__dirname, 'src', 'index.ts'), // склеиваем участки пути (__dirname - текущая папка)
+		build: path.resolve(__dirname, 'build'), //куда сохраняется
+		html: path.resolve(__dirname, 'public', 'index.html'), // при создании index.html в build используем шаблон из папки public файл index.html
+	}
+
+	const PORT = env.port || 3000 // в первую очередь пытаемся получить переменные, но если они не заданы - используем значения по умолчанию
+	const mode = env.mode || 'development'
+	const isDev = mode === 'development'
+
+	const config: webpack.Configuration = buildWebpackConfig({
+		mode,
+		paths,
+		isDev,
+		port: PORT,
+	})
+	return config
 }
-
-const mode = 'development'
-const isDev = mode === 'development'
-
-const config: webpack.Configuration = buildWebpackConfig({
-	mode: 'development',
-	paths,
-	isDev
-})
-
-
-export default config
 
