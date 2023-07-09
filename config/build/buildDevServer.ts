0903adb8ef55d907e8ec41aa8266ad71088d0857
Violetta <viol.fedoseenko@yandex.ru>
2023-07-09 16:35:09 +0300

    implemented routing and code splitting

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,6 +4,8 @@ import type {Configuration as DevServerConfiguration} from 'webpack-dev-server'
 export function buildDevServer(options:BuildOptions): DevServerConfiguration {
 	return {
 		port: options.port,
-		open: true
+		open: true,
+		//свойство позволяет проксировать запросы через корневую страницу index page
+		historyApiFallback: true,
 	}
 }
