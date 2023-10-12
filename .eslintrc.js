cd0c77e655c21646a6c0db572217df4e91c32dea
Violetta <viol.fedoseenko@yandex.ru>
2023-10-12 17:45:37 +0300

    add jest, add first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,89 @@
 module.exports = {
+
     env: {
+
         browser: true,
+
         es2021: true,
+
+        jest: true,
+
     },
+
     extends: [
+
         'plugin:react/recommended',
+
         'airbnb',
+
         'plugin:i18next/recommended',
+
     ],
+
     parser: '@typescript-eslint/parser',
+
     parserOptions: {
+
         ecmaVersion: 'latest',
+
         sourceType: 'module',
+
     },
+
     plugins: [
+
         'react',
+
         '@typescript-eslint',
+
         'i18next',
+
     ],
+
     rules: {
+
         'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+
         'react/jsx-indent': [2, 4], // 2- будет бросаться error при несоблюдении правила , 4 кол-во пробелов
+
         indent: [2, 4],
+
         'react/jsx-indent-props': [2, 4],
+
         'react/react-in-jsx-scope': 'off',
+
         'import/no-unresolved': 'off',
+
         'import/prefer-default-export': 'off',
+
         'no-unused-vars': 'warn',
+
         'react/require-default-props': 'off',
+
         'react/jsx-props-no-spreading': 'warn',
+
         'react/function-component-definition': 'off',
+
         'no-shadow': 'off',
+
         'import/extensions': 'off',
+
         'import/no-extraneous-dependencies': 'off',
+
         'no-underscore-dangle': 'off',
+
         'max-len': ['error', { code: 150 }],
+
         'i18next/no-literal-string': ['error', { markupOnly: true }], // следит только за отсутствием перевродов внутри jsx
+
+        'linebreak-style': 'off',
+
     },
+
     globals: {
+
         __IS_DEV__: true,
+
     },
+
 };
--- src/widgets/SideBar/ui/SideBar/SideBar.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
+import { useTranslation } from 'react-i18next';
 import cls from './SideBar.module.scss';
 
 interface SideBarProps {
@@ -11,13 +12,15 @@ interface SideBarProps {
 export function SideBar({ className }: SideBarProps) {
     const [collapsed, setCollapsed] = useState(false);
 
+    const { t } = useTranslation();
+
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
 
     return (
         <div className={classNames(cls.sideBar, { [cls.collapsed]: collapsed }, [className])}>
-            <button type="button" onClick={onToggle}>toggle</button>
+            <button type="button" onClick={onToggle}>{t('toggle')}</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher />
