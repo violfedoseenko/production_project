03d5e1ec7cf891d801416dc31aad1d200d41a6fa
Violetta <viol.fedoseenko@yandex.ru>
2023-11-21 14:24:44 +0300

    fix screens, add visual tool for screenshot testing

--- src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
@@ -1,14 +1,14 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { SideBar } from './SideBar';
-import { renderWithTranslation } from '../../../../shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from '../../../../shared/lib/tests/componentRender/componentRender';
 
 describe('sidebar', () => {
     test('render test', () => {
-        renderWithTranslation(<SideBar />);
+        componentRender(<SideBar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('toggle test', () => {
-        renderWithTranslation(<SideBar />);
+        componentRender(<SideBar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         fireEvent.click(toggleBtn);
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
